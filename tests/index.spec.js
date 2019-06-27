f1427ef06bd04745909e151969cc4483b90cf98c
Kelvin Esegbona <kevoesegbona@gmail.com>
2019-06-27 06:54:16 +0100

    166816199-chore(swagger.yaml, index.js): set up api documentation using swagger
      - Install swagger packages
      - Add swagger.yaml file
      - Set up swagger api-docs
    
    [Finishes #166816199]

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,22 +1,26 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
-const expect = chai.expect;
 import { app } from '../index';
+
+const { expect } = chai;
 
 chai.use(chaiHttp);
 
 describe('test run', () => {
-	it('foo is string', () => {
-		expect('foo').to.be.a('string');
-	});
+  it('foo is string', () => {
+    expect('foo').to.be.a('string');
+  });
 });
 
 describe('test chai-http', () => {
-	it('run chai', (done) => {
-		chai.request(app).get('/').end((req, res) => {
-			expect(res.body).to.be.a('object');
-			expect(res.body).to.have.property('message');
-			done();
-		});
-	});
+  it('run chai', (done) => {
+    chai
+      .request(app)
+      .get('/')
+      .end((req, res) => {
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.have.property('message');
+        done();
+      });
+  });
 });
