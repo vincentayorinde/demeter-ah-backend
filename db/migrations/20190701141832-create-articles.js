44ae4e7d0993ab7f4dd8bf3f89c74a66847a843d
ewere <ewisco316@yahoo.com>
2019-07-01 15:58:53 +0100

    167012648-feat(model): create article model
    - create article model
    - create article migration file
    - install sequelize-slugify package
    
    [Delivers #167012648]

---
+++ db/migrations/20190701141832-create-articles.js
@@ -0,0 +1,34 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Articles', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    title: {
+      type: Sequelize.STRING
+    },
+    description: {
+      type: Sequelize.TEXT
+    },
+    slug: {
+      type: Sequelize.STRING
+    },
+    body: {
+      type: Sequelize.TEXT
+    },
+    images: {
+      type: Sequelize.STRING
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+  }),
+  down: queryInterface => queryInterface.dropTable('articles')
+};
